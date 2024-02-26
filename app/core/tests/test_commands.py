db2418450a978d0c931f70bac02bbf1237331514
trungnguyenduc150195@gmail.com <>
2024-02-26 10:37:31 +0700

    add tesst db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -11,7 +11,7 @@ class CommandTests(SimpleTestCase):
 
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready"""
-        patched_check.return_value = False
+        patched_check.return_value = True
         
         call_command('wait_for_db')
 
@@ -21,7 +21,7 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_delay(self,patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
-        [OperationalError] * 3  + [True]
+        [OperationalError] * 3 + [True]
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
