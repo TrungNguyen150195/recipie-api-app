101085dac8bf06b1f0195bc1099f53b5675116c5
trungnguyenduc150195@gmail.com <>
2024-02-28 13:11:19 +0700

    add more test case

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,29 @@
-from django.db import models    # noqa
+"""
+DB models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+class userManager(BaseUserManager):
+    """Manage for users."""
+
+    def creat_user(self, email, password=None, **extra_fileds):
+        """create, save and return as new user."""
+        user = self.model(email=self.normalize_email(email), **extra_fileds)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    object = userManager()
+
+    USERNAME_FIELD = 'email'
